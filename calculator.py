7232cffd4e96e308670de331b6fade5097275d60
Kam Rice <Kameron.Rice@MyManatee.org>
2026-04-08 14:38:34 -0400

    modified calculator p1

--- calculator.py
+++ calculator.py
@@ -1,11 +1,86 @@
-"""
-calculator.py
-- Defines functions used to create a simple calculator
+# https://github.com/KamRice/lab11-KR-PA
+# Partner 1: Kameron Rice
+# Partner 2: Palmer Ackerbloom
 
-One function per operation, in order.
-"""
+import math
+
+
 # First example
-def add(a, b): 
-    pass
+def add(a, b):
+    if not isinstance(a, int):
+        print(f"TypeError for input: {a}")
+        return None
+    if not isinstance(b, int):
+        print(f"TypeError for input: {b}")
+        return None
+
+    return a + b
+
+
+def subtract(a, b):
+    if not isinstance(a, int):
+        print(f"TypeError for input: {a}")
+        return None
+    if not isinstance(b, int):
+        print(f"TypeError for input: {b}")
+        return None
+
+    return a - b
+
+
+def mul(a, b):
+    if not isinstance(a, int):
+        print(f"TypeError for input: {a}")
+        return None
+    if not isinstance(b, int):
+        print(f"TypeError for input: {b}")
+        return None
+
+    return a * b
+
+
+def div(a, b):
+    if not isinstance(a, int):
+        print(f"TypeError for input: {a}")
+        return None
+    if not isinstance(b, int):
+        print(f"TypeError for input: {b}")
+        return None
+
+    try:
+        return a / b
+
+    except ZeroDivisionError:
+        print("Attempted to divide by 0.")
+
+
+def logarithm(a, b):
+    if not isinstance(a, int):
+        print(f"TypeError for input: {a}")
+        return None
+    if not isinstance(b, int):
+        print(f"TypeError for input: {b}")
+        return None
+
+    try:
+
+        if a <= 0 or b <= 0:
+            raise ValueError("Inputs cannot be negative.")
+
+        return 1 / math.log(a, b)
+
+    except ValueError:
+        print(f"{ValueError.__name__}: Inputs cannot be negative.")
+
+
+def exp(a, b):
+    if not isinstance(a, int):
+        print(f"TypeError for input: {a}")
+        return None
+    if not isinstance(b, int):
+        print(f"TypeError for input: {b}")
+        return None
+
+    return a ** b
 
 
